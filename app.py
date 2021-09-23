55082434393643715df4f12b9c06b23a71a67875
amirkh75 <amirp.1375@gmail.com.com>
2021-09-23 16:57:10 -0400

    add some prints for better view about route.

--- app.py
+++ app.py
@@ -9,21 +9,32 @@ def app(environ, start_response):
     return iter([response_body])
 """
 
+from webob.descriptors import parse_int_safe
 from api import API
 
 app = API()
 
+print('line 17.')
+
+for key in app.routes:
+    print(key + ': ' + str(app.routes[key]))
 
 @app.route('/')
 def home(request, response):
     """ explains the current..."""
     response.text = '<h2>Hello from the HOME page.<\h2>'
 
+for key in app.routes:
+    print(key + ': ' + str(app.routes[key]))
+
+print('line 30.')
+
 @app.route('/about')
 def about(request, response):
     """ explains the current..."""
     response.text = '<h2>Hello from the ABOUT page.<\h2>'
 
+print('line 37.')
 
 for key in app.routes:
     print(key + ': ' + str(app.routes[key]))
